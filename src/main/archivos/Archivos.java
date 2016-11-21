0860dbb7e27aa7867a34eaa46d75bc0980294812
GalaxiasKyklos <saul_enrique-7285-96@hotmail.com>
2016-11-21 13:01:26 -0600

    Archivos v0

---
+++ src/main/archivos/Archivos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.archivos;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author Saúl
+ */
+public class Archivos {
+    private File files[];
+    private String currentStr[];
+    private File currentFile;
+    private int innerIndex;
+    private int outerIndex;
+    
+    public Archivos() {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos de texto", "txt");
+        chooser.setFileFilter(filter);
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int c = chooser.showOpenDialog(null);
+        
+        if (c == JFileChooser.APPROVE_OPTION) {
+            File folder = chooser.getCurrentDirectory();
+            files = folder.listFiles();
+        }
+        
+        innerIndex = 0;
+        outerIndex = 0;
+    }
+    
+    public String nextString() {
+        
+    }
+}
